5f3acccb97f050905eba762ea2b793394790f7c8
D. Fyffe <fyffed@netzero.net>
2025-01-07 07:37:01 -0500

    Update Github user parameters

--- selfdrive/ui/qt/widgets/kisapilot.cc
+++ selfdrive/ui/qt/widgets/kisapilot.cc
@@ -524,13 +524,13 @@ OpenpilotUserEnv::OpenpilotUserEnv() : ButtonControl(tr("Get Your Params"), "", 
       QString userid = InputDialog::getText(tr("Input your Git ID"), this, "github.com/<your id>/openpilot_user/main/user_params.txt", false, 1, "");
       if (userid.length() > 0) {
         getUserID(userid);
-        QString repoid = InputDialog::getText(tr("Input your repository"), this, "github.com/"+userid, false, 1, "");
+        QString repoid = InputDialog::getText(tr("Input your repository"), this, "github.com/"+userid, false, 1, "openpilot_user");
         if (repoid.length() > 0) {
           getRepoID(repoid);
-          QString branchid = InputDialog::getText(tr("Input your branch"), this, "github.com/"+userid+"/"+repoid, false, 1, "");
+          QString branchid = InputDialog::getText(tr("Input your branch"), this, "github.com/"+userid+"/"+repoid, false, 1, "main");
           if (branchid.length() > 0) {
             getBranchID(branchid);
-            QString fileid = InputDialog::getText(tr("Input your file"), this, "github.com/"+userid+"/"+repoid+"/"+branchid, false, 1, "");
+            QString fileid = InputDialog::getText(tr("Input your file"), this, "github.com/"+userid+"/"+repoid+"/"+branchid, false, 1, "user_params.txt");
             if (fileid.length() > 0) {
               getFileID(fileid);
               githubbranch = branchid;
